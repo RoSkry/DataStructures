dc2a2c2f63dd6eda2b87d52e40d8236b8dde327b
Rostyslav Skrypnyk <skrypnik-r@ukr.net>
2020-05-16 23:19:33 +0300

    Add doubly linked list with functionality

---
+++ DoublyLinkedList/Model/DoublyLinkedList.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoublyLinkedList.Model
+{
+    public class DoublyLinkedList<T> : IEnumerable<T>
+    {
+        public Item<T> Head { get; set; }
+        public Item<T> Tail { get; set; }
+        public int Count { get; set; }
+
+        public DoublyLinkedList() { }
+
+        public DoublyLinkedList(T data)
+        {
+            var item = new Item<T>(data);
+            Head = item;
+            Tail = item;
+            Count = 1;
+        }
+
+        public void Add(T data)
+        {
+            var item = new Item<T>(data);
+            if (Head == null)
+            {
+                Head = item;
+            }
+            else
+            {
+                Tail.Next = item;
+                item.Previous = Tail;
+            }
+
+            Tail = item;
+            Count++;
+        }
+
+        public void Remove(T data)
+        {
+            if (Head.Data.Equals(data))
+            {
+                Head = Head.Next;
+                Head.Previous = null;
+                Count--;
+                return;
+            }
+            if (Tail.Data.Equals(data))
+            {
+                Tail = Tail.Previous;
+                Tail.Next = null;
+                Count--;
+                return;
+            }
+
+            var curent = Head;
+            while (curent != null)
+            {
+                if (curent.Data.Equals(data))
+                {
+                    curent.Previous.Next = curent.Next;
+                    curent.Next.Previous = curent.Previous;
+                    Count--;
+                    return;
+                }
+                curent = curent.Next;
+            }
+        }
+
+        public void AddFirst(T data)
+        {
+            var item = new Item<T>(data);
+            var current = Head;
+            item.Next = current;
+            Head = item;
+            if (Count == 0)
+            {
+                Tail = Head;
+            }
+            else
+            {
+                current.Previous = item;
+            }
+            Count++;
+        }
+
+        public void AddAfter(T target, T data)
+        {
+            if (Head != null)
+            {
+                var current = Head;
+                while (current != null)
+                {
+                    if (current.Data.Equals(target))
+                    {
+                        var item = new Item<T>(data);
+                        item.Next = current.Next;
+                        current.Next = item;
+                        item.Previous = current;
+                        Count++;
+                        return;
+                    }
+                    current = current.Next;
+                }
+            }
+        }
+
+        public bool Contains(T data)
+        {
+            var current = Head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void ClearAll()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public DoublyLinkedList<T> Reverse()
+        {
+            var result = new DoublyLinkedList<T>();
+            var current = Tail;
+            while (current != null)
+            {
+                result.Add(current.Data);
+                current = current.Previous;
+            }
+
+            return result;
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            var current = Head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+
+    }
+}
---
+++ DoublyLinkedList/Program.cs
@@ -0,0 +1,53 @@
+﻿using DoublyLinkedList.Model;
+using System;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doublyList = new DoublyLinkedList<int>();
+            doublyList.Add(1);
+            doublyList.Add(2);
+            doublyList.Add(3);
+            doublyList.Add(4);
+            doublyList.Add(5);
+
+            foreach (var item in doublyList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            doublyList.Remove(5);
+            foreach (var item in doublyList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            var reverse = doublyList.Reverse();
+            foreach (var item in reverse)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            doublyList.AddFirst(24);
+            foreach (var item in doublyList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            doublyList.AddAfter(4, 2020);
+            foreach (var item in doublyList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
