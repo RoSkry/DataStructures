891f41eae9b9ec7009d6ed10ee635bddfb8914a7
Rostyslav Skrypnyk <skrypnik-r@ukr.net>
2020-05-16 12:56:55 +0300

    Add project files.

---
+++ LinkedList/Model/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList.Model
+{
+    public class Item<T>
+    {
+        private T data = default(T);
+        private Item<T> next = null;
+    }
+}
