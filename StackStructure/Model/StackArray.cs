7a4f5294941f1f57a5ee9330979a643bc7e6b608
Rostyslav Skrypnyk <skrypnik-r@ukr.net>
2020-05-17 17:33:07 +0300

    Add stacke based on array with functionality

---
+++ StackStructure/Model/StackArray.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackStructure.Model
+{
+    public class StackArray<T>
+    {
+        private T[] items;
+        private int count;
+        public int CountInArray { get; } = 10;
+        public StackArray()
+        {
+            items = new T[CountInArray];
+        }
+        public StackArray(int lenght)
+        {
+            items = new T[lenght];
+        }
+        public bool IsEmpty
+        {
+            get { return count == 0; }
+        }
+
+        public void Push(T item)
+        {
+            if (count == items.Length)
+                Resize(items.Length + 10);
+            items[count++] = item;
+        }
+
+        private void Resize(int max)
+        {
+            T[] tempItems = new T[max];
+            for (int i = 0; i < count; i++)
+            {
+                tempItems[i] = items[i];
+            }
+            items = tempItems;
+        }
+
+        public T Pop()
+        {
+            if(IsEmpty)
+                throw new InvalidOperationException("Stack is empty");
+            T item = items[--count];
+            items[count] = default(T);
+
+            if (count > 0 && count < items.Length - 10)
+                Resize(items.Length - 10);
+
+            return item;
+        }
+
+        public T Peek()
+        {
+            return items[count - 1];
+        }
+    }
+}
---
+++ StackStructure/Program.cs
@@ -0,0 +1,31 @@
+﻿using StackStructure.Model;
+using System;
+using System.Collections;
+
+namespace StackStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stackArray = new StackArray<int>(3);
+
+            stackArray.Push(1);
+            stackArray.Push(2);
+            stackArray.Push(3);
+            stackArray.Push(4);
+
+            var item = stackArray.Pop();
+            Console.WriteLine(item);
+
+            item = stackArray.Peek();
+            Console.WriteLine(item);
+
+            while (!stackArray.IsEmpty)
+            {
+                var item1 = stackArray.Pop();
+                Console.WriteLine(item1);
+            }
+        }
+    }
+}
