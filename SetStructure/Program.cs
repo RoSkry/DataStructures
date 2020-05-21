2d0db0d61f31a598f5a12ac21a359af0d22a8d00
Rostyslav Skrypnyk <skrypnik-r@ukr.net>
2020-05-21 23:36:04 +0300

    Implement Set based on List with functionality

---
+++ SetStructure/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SetStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setList1 = new SetList<int>(new int[] { 1, 2, 3, 4, 5 });
+            var setList2 = new SetList<int>(new int[] { 4, 5, 6, 7, 8 });
+            var setList3 = new SetList<int>(new int[] { 3,4,5});
+
+            Console.Write("Union: ");
+            foreach (var item in setList1.Union(setList2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Intersection: ");
+            foreach (var item in setList1.Intersection(setList2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Difference A\\B: ");
+            foreach (var item in setList1.Difference(setList2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Difference B\\A: ");
+            foreach (var item in setList2.Difference(setList1))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("C Subset A: ");
+            Console.Write(setList1.Subset(setList3));
+            Console.WriteLine();
+
+            Console.Write("A Subset C: ");
+            Console.Write(setList3.Subset(setList1));
+            Console.WriteLine();
+
+            Console.Write("B Subset A: ");
+            Console.Write(setList3.Subset(setList1));
+            Console.WriteLine();
+
+            Console.Write("Symmetric Difference: ");
+            foreach (var item in setList1.SymmetricDifference(setList2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SetStructure/SetList.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace SetStructure
+{
+    public class SetList<T> : IEnumerable
+    {
+        private List<T> _items = new List<T>();
+
+        public int Count => _items.Count;
+
+        public SetList()
+        {
+
+        }
+
+        public SetList(IEnumerable<T> items)
+        {
+            _items = items.ToList();
+        }
+        public SetList(T item)
+        {
+            _items.Add(item);
+        }
+
+        public void Add(T item)
+        {
+
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            if (!_items.Contains(item))
+            {
+                _items.Add(item);
+            }
+        }
+
+        public void Remove(T item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            _items.Remove(item);
+        }
+
+        public SetList<T> Union(SetList<T> set)
+        {
+            // return new SetList<T>(_items.Union(set._items));
+
+            if (set == null)
+            {
+                throw new ArgumentNullException(nameof(set));
+            }
+
+            var result = new SetList<T>();
+
+            foreach (var item in _items)
+            {
+                result.Add(item);
+            }
+
+            foreach (var item in set._items)
+            {
+                result.Add(item);
+            }
+            return result;
+        }
+
+        public SetList<T> Intersection(SetList<T> set)
+        {
+            //   return new SetList<T>(items.Intersect(set.items));
+            if (set == null)
+            {
+                throw new ArgumentNullException(nameof(set));
+            }
+            var result = new SetList<T>();
+
+            if (this.Count < set.Count)
+            {
+                foreach (var item in this._items)
+                {
+                    if (set._items.Contains(item))
+                    {
+                        result.Add(item);
+                    }
+                }
+            }
+
+            else
+            {
+                foreach (var item in set._items)
+                {
+                    if (this._items.Contains(item))
+                    {
+                        result.Add(item);
+                    }
+                }
+            }
+
+
+            return result;
+        }
+
+        public SetList<T> Difference(SetList<T> set)
+        {
+            //   return new SetList<T>(items.Except(set.items));
+            if (set == null)
+            {
+                throw new ArgumentNullException(nameof(set));
+            }
+            var result = new SetList<T>(_items);
+
+            foreach (var item in set._items)
+            {
+                result.Remove(item);
+            }
+
+            return result;
+        }
+
+        public bool Subset(SetList<T> set)
+        {
+            //   return set.items.All(i => items.Contains(i));
+
+            if (set == null)
+            {
+                throw new ArgumentNullException(nameof(set));
+            }
+
+            foreach (var item in set._items)
+            {
+                var equals = false;
+                if(_items.Contains(item))
+                {
+                    equals = true;
+                    break;
+                }
+                if (!equals)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public SetList<T> SymmetricDifference(SetList<T> set)
+        {
+            //  return new SetList<T>(items.Except(set.items).Union(set.items.Except(items));
+
+            if (set == null)
+            {
+                throw new ArgumentNullException(nameof(set));
+            }
+
+            var result = new SetList<T>();
+
+            foreach (var item in _items)
+            {
+                var equals = false;
+
+                if(set._items.Contains(item))
+                {
+                    equals = true;
+                   
+                }
+
+                if (!equals)
+                {
+                    result.Add(item);
+                }
+            }
+
+            foreach (var item in set._items)
+            {
+                var equals = false;
+                if (_items.Contains(item))
+                {
+                    equals = true;
+
+                }
+
+                if (!equals)
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+    }
+}
