18ee76a0c2f275f9be44c7e4b1940ff99e5155de
Rostyslav Skrypnyk <skrypnik-r@ukr.net>
2020-05-24 21:57:29 +0300

    Implement Binary Search Tree

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class Node<T> : IComparable<T>, IComparable where T : IComparable
+    {
+        public T Data { get; private set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+            if (node.Data.CompareTo(Data) == -1)
+            {
+                if (Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(data);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(data);
+                }
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Node<T> item)
+            {
+                return Data.CompareTo(item);
+            }
+            else
+            {
+                throw new ArgumentException("Different types");
+            }
+
+        }
+
+        public int CompareTo([AllowNull] T other)
+        {
+            return Data.CompareTo(other);
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class Tree<T> where T : IComparable, IComparable<T>
+    {
+        public Node<T> Root { get; private set; }
+        public int Count { get; private set; }
+
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+
+            if (Root == null)
+            {
+                Root = new Node<T>(data);
+                Count = 1;
+                return;
+            }
+
+            Root.Add(data);
+            Count++;
+        }
+
+        public List<T> PreOrder()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+
+            return PreOrder(Root);
+        }
+
+        public List<T> PostOrder()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+
+            return PostOrder(Root);
+        }
+
+        public List<T> InOrder()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+
+            return InOrder(Root);
+        }
+
+        private List<T> PreOrder(Node<T> node)
+        {
+            var list = new List<T>();
+            if (node != null)
+            {
+                list.Add(node.Data);
+
+                if (node.Left != null)
+                {
+                    list.AddRange(PreOrder(node.Left));
+                }
+
+                if (node.Right != null)
+                {
+                    list.AddRange(PreOrder(node.Right));
+                }
+            }
+
+            return list;
+        }
+
+        private List<T> PostOrder(Node<T> node)
+        {
+            var list = new List<T>();
+            if (node != null)
+            {
+                if (node.Left != null)
+                {
+                    list.AddRange(PostOrder(node.Left));
+                }
+
+                if (node.Right != null)
+                {
+                    list.AddRange(PostOrder(node.Right));
+                }
+
+                list.Add(node.Data);
+            }
+
+            return list;
+        }
+
+        private List<T> InOrder(Node<T> node)
+        {
+            var list = new List<T>();
+            if (node != null)
+            {
+                if (node.Left != null)
+                {
+                    list.AddRange(InOrder(node.Left));
+                }
+
+                list.Add(node.Data);
+
+                if (node.Right != null)
+                {
+                    list.AddRange(InOrder(node.Right));
+                }
+            }
+
+            return list;
+        }
+
+
+    }
+}
